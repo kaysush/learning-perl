432857b828063d4ca56fbe953175fe4ef6f530b0
kaysush <0120sushil@gmail.com>
2013-07-03 09:34:45 +0530

    Comment added for silly mistake

--- chapter-4/2.pl
+++ chapter-4/2.pl
@@ -13,6 +13,6 @@ sub total {
 
 my @array = (1..1000);
 my ($first) = @array;
-my $last = @array;
+my $last = @array; #Since the numbers are increasing by 1.
 my $total = &total( @array );
 print "Total of nos. from $first to $last is $total \n";
