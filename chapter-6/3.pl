c8832bbc02d81ff0ad7686ec8567ae8bd9c2a25d
kaysush <0120sushil@gmail.com>
2013-07-05 09:57:01 +0530

    Alignment only for keys

--- chapter-6/3.pl
+++ chapter-6/3.pl
@@ -8,8 +8,6 @@ my @keys = sort keys %ENV;
 foreach (@keys) {
 	my $key_length = length $_;
 	my $key_width = 5 + $key_length;
-	my $value_length = length $ENV{$_};
-	my $value_width = 5 + $value_length + $key_length;
 	#print "$_ => $ENV{$_} \n";
-	printf "%${key_width}s %${value_length}s \n" , $_ , $ENV{$_};
+	printf "%${key_width}s => %s\n" , $_ , $ENV{$_};
 }
