2a8ad58ed51e53909610b31ccc527dcc1dd5816d
kaysush <0120sushil@gmail.com>
2013-07-06 10:29:50 +0530

    Using character class for more efficiency

--- chapter-7/2.pl
+++ chapter-7/2.pl
@@ -3,7 +3,7 @@
 use strict;
 
 while(<>) {
-	if(/(f|F)red/) { # use /(f|F)red/ if you want match both fred and Fred
+	if(/[fF]red/) { # use /(f|F)red/ if you want match both fred and Fred
 		print "$_\n";
 	}
 }
