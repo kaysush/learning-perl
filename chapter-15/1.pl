2954325df49c24de4d6248db1075dc9f53d0fb5a
kaysush <0120sushil@gmail.com>
2013-07-16 16:03:19 +0530

    Moving last out of default because of warnings

--- chapter-15/1.pl
+++ chapter-15/1.pl
@@ -4,6 +4,7 @@ use strict;
 use 5.010;
 
 my $rand = int(1 + rand 100);
+my $count = 0;
 
 say("The secret no. is $rand") if $ARGV[0]; # if any value is passed from command line then it shows the number
 
@@ -12,6 +13,7 @@ while(1){
 	given($value){
 		when ($_ < $rand) 	{ say ("Too small."); }
 		when ($_ > $rand) 	{ say ("Too Big."); }
-		default 			{ say("Perfect");last; }
+		default 			{ say("Perfect");$count++; }
 	}
+	last if $count;
 }
